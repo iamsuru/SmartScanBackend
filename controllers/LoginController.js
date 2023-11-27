068b7a9374febc0326c4f84e8c1fcd5a06341247
iamsuru <suryanshshrivastava6364@gmail.com>
2023-11-27 14:33:51 +0530

    0.1.1

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,5 +1,6 @@
 const Auth = require("../src/models/registerSchema")
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const Login = async (req,res) => {
     console.log('called');
@@ -13,7 +14,9 @@ const Login = async (req,res) => {
         if (!passwordMatch) {
             return res.status(401).json({ message: 'Invalid Credentials' })
         }
-        res.status(200).json({ message: 'Authentication successful', auth: auth })
+        const token = await jwt.sign({user : auth},'asdfghjklmnbvcxz8520', {expiresIn: '1h'})
+        res.cookie('auth-token',token,  { maxAge: 900000, httpOnly: true, secure:true })
+        res.status(200).json({ message: 'Authentication successful', token : token, auth: auth })
     } catch (err) {
         res.status(500).json({ message: `Internal Server Error ${err}` })
     }
