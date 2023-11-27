4466afc6b1e7052555b1195c290e05974a837ec0
iamsuru <suryanshshrivastava6364@gmail.com>
2023-11-27 09:23:34 +0530

    0.1.1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const AuthRouter = require('./routes/Auth')
-const RegisterRouter = require('./routes/Register')
+const AuthRouter = require('./routes/route')
+// const RegisterRouter = require('./routes/Register')
 require('./src/db/config')
 
 const app = express()
@@ -10,9 +10,16 @@ const PORT = process.env.PORT || 2000
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
+app.get('/',(req,res)=>{
+    res.end('started')
+})
+
+app.get('/api/login',(req,res)=>{
+    res.end('started login')
+})
 
 app.use('/api', AuthRouter)
-app.use('/api',RegisterRouter)
+app.use('/api',AuthRouter)
 
 app.listen(PORT, () => {
     console.log(`Connection established on ${PORT}`)
--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const LoginController = require('../controllers/LoginController')
+const RegisterController = require('../controllers/RegisterController')
 const router = express.Router()
 
 router.post('/login', LoginController.Login)
+router.post('/register', RegisterController.Register)
 
 module.exports = router
